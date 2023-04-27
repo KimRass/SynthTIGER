76e61856fcd6d3bbc8f12267a98caa18f920dda1
KimRass <rmx1000@yonsei.ac.kr>
2023-04-27 18:57:34 +0900

    Minor changes

--- synthtiger/run.sh
+++ synthtiger/run.sh
@@ -2,9 +2,9 @@
 python3 synthtiger/main.py\
     --template_path="examples/synthtiger/template.py"\
     --template_name="SynthTiger"\
-    --config="examples/synthtiger/config_horizontal.yaml"\
+    --config="examples/config.yaml"\
     --output="/Users/jongbeomkim/Documents/synthtiger"\
-    --count=1\
+    --count=50\
     --n_workers=4\
     --seed=77\
     --verbose\
